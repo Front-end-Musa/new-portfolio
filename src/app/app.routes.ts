8bebf66e2621918be65ec41268888219fbf00354
Musa Rufatuly <rufatulymusa567@gmail.com>
2023-12-31 01:06:16 +0600

    updating

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ServicesComponent } from './components/services/services.component';
 
 export const routes: Routes = [
- { path: '', component: MainPageComponent }
+ { path: '', component: MainPageComponent },
+ { path: 'about-me', component: AboutMeComponent },
+ { path: 'projects', component: ProjectsComponent }, 
+ { path: 'services', component: ServicesComponent },
 ];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,4 @@
-import { AboutMeComponent } from './../about-me/about-me.component';
 import { Component } from '@angular/core';
-import { MainPageComponent } from '../main-page/main-page.component';
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  { path: '', component: MainPageComponent },
-  { path: '/about-me', component: AboutMeComponent },
-];
 
 @Component({
   selector: 'app-header',
